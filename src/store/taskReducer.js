0f55430e37408ac9fa79cdcb4474115c0ccf0a01
DariaBrusnitsina <dbrusnitsina@gmail.com>
2023-03-13 20:05:44 +0300

    feat: implement gh-pages

---
+++ src/store/taskReducer.js
@@ -0,0 +1,17 @@
+export function taskReducer(state =[], action) {
+    switch (action.type) {
+      case "task/updated": {
+        const newArray =[...state]
+        const elementIndex = newArray.findIndex((el) => el.id === action.payload.id)
+        newArray[elementIndex] = {...newArray[elementIndex], ...action.payload}
+        return newArray
+      }
+      case "task/deleted": {
+        const newArray =[...state]
+        return newArray.filter(el => el.id !== action.payload.id);
+      }
+
+      default:
+        return state;
+    }
+}
